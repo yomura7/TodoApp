3789d19512a072f65a805d1e4f458b79563df739
Yohei Omura <yomura7@gmail.com>
2019-12-26 01:54:51 +0900

    add toggling todo/done

--- out/production/TodoApp/TodoApp.js
+++ out/production/TodoApp/TodoApp.js
@@ -4,83 +4,96 @@ if (typeof kotlin === 'undefined') {
 var TodoApp = function (_, Kotlin) {
   'use strict';
   var ensureNotNull = Kotlin.ensureNotNull;
-  var println = Kotlin.kotlin.io.println_s8jyv4$;
   var throwCCE = Kotlin.throwCCE;
   var Unit = Kotlin.kotlin.Unit;
-  var Kind_CLASS = Kotlin.Kind.CLASS;
   var equals = Kotlin.equals;
+  var Kind_CLASS = Kotlin.Kind.CLASS;
   function main$lambda$lambda(closure$todoList) {
     return function (it) {
       var tmp$;
-      println('click!');
       var input = Kotlin.isType(tmp$ = document.querySelector('#input'), HTMLInputElement) ? tmp$ : throwCCE();
       closure$todoList.addTodo_61zpoe$(input.value);
       input.value = '';
       return Unit;
     };
   }
+  function main$lambda$lambda_0(closure$todoList) {
+    return function (it) {
+      closure$todoList.clearTodoList();
+      return Unit;
+    };
+  }
+  function main$lambda$lambda_1(closure$todoList) {
+    return function (it) {
+      closure$todoList.clearDoneList();
+      return Unit;
+    };
+  }
   function main$lambda(it) {
-    var tmp$;
+    var tmp$, tmp$_0, tmp$_1;
     var todoUl = document.querySelector('#todo_ul');
     var doneUl = document.querySelector('#done_ul');
     var todoList = new TodoManager(ensureNotNull(todoUl), ensureNotNull(doneUl));
-    (tmp$ = document.querySelector('#button')) != null ? (tmp$.addEventListener('click', main$lambda$lambda(todoList)), Unit) : null;
+    todoList.addTodo_61zpoe$('\u4ECA\u6708\u306E\u5BB6\u8CC3\u3092\u652F\u6255\u3046');
+    todoList.addDone_61zpoe$('\u6D17\u5264\u3092\u8CB7\u3046');
+    todoList.addDone_61zpoe$('\u672C\u68DA\u3092\u6383\u9664\u3059\u308B');
+    (tmp$ = document.querySelector('#add_button')) != null ? (tmp$.addEventListener('click', main$lambda$lambda(todoList)), Unit) : null;
+    (tmp$_0 = document.querySelector('#todo_clear_button')) != null ? (tmp$_0.addEventListener('click', main$lambda$lambda_0(todoList)), Unit) : null;
+    (tmp$_1 = document.querySelector('#done_clear_button')) != null ? (tmp$_1.addEventListener('click', main$lambda$lambda_1(todoList)), Unit) : null;
     return Unit;
   }
   function main() {
     document.addEventListener('DOMContentLoaded', main$lambda);
   }
-  function Task(id, name, isChecked) {
-    this.id = id;
-    this.name = name;
-    this.isChecked = isChecked;
-  }
-  Task.$metadata$ = {
-    kind: Kind_CLASS,
-    simpleName: 'Task',
-    interfaces: []
-  };
-  Task.prototype.component1 = function () {
-    return this.id;
-  };
-  Task.prototype.component2 = function () {
-    return this.name;
-  };
-  Task.prototype.component3 = function () {
-    return this.isChecked;
-  };
-  Task.prototype.copy_cqw3wz$ = function (id, name, isChecked) {
-    return new Task(id === void 0 ? this.id : id, name === void 0 ? this.name : name, isChecked === void 0 ? this.isChecked : isChecked);
-  };
-  Task.prototype.toString = function () {
-    return 'Task(id=' + Kotlin.toString(this.id) + (', name=' + Kotlin.toString(this.name)) + (', isChecked=' + Kotlin.toString(this.isChecked)) + ')';
-  };
-  Task.prototype.hashCode = function () {
-    var result = 0;
-    result = result * 31 + Kotlin.hashCode(this.id) | 0;
-    result = result * 31 + Kotlin.hashCode(this.name) | 0;
-    result = result * 31 + Kotlin.hashCode(this.isChecked) | 0;
-    return result;
-  };
-  Task.prototype.equals = function (other) {
-    return this === other || (other !== null && (typeof other === 'object' && (Object.getPrototypeOf(this) === Object.getPrototypeOf(other) && (Kotlin.equals(this.id, other.id) && Kotlin.equals(this.name, other.name) && Kotlin.equals(this.isChecked, other.isChecked)))));
-  };
   function TodoManager(todoUl, doneUl) {
     this.todoUl_0 = todoUl;
     this.doneUl_0 = doneUl;
   }
-  function TodoManager$addTodo$lambda(it) {
-    println('click on ' + it);
-    return Unit;
+  function TodoManager$addTodo$lambda(this$TodoManager, closure$li, closure$name) {
+    return function (it) {
+      this$TodoManager.todoUl_0.removeChild(closure$li);
+      this$TodoManager.addDone_61zpoe$(closure$name);
+      return Unit;
+    };
   }
   TodoManager.prototype.addTodo_61zpoe$ = function (name) {
-    if (equals(name, '')) {
+    if (equals(name, ''))
       return;
-    }
     var li = document.createElement('li');
     li.textContent = name;
-    li.addEventListener('click', TodoManager$addTodo$lambda);
+    li.addEventListener('click', TodoManager$addTodo$lambda(this, li, name));
     this.todoUl_0.appendChild(li);
+  };
+  function TodoManager$addDone$lambda(this$TodoManager, closure$li, closure$name) {
+    return function (it) {
+      this$TodoManager.doneUl_0.removeChild(closure$li);
+      this$TodoManager.addTodo_61zpoe$(closure$name);
+      return Unit;
+    };
+  }
+  TodoManager.prototype.addDone_61zpoe$ = function (name) {
+    if (equals(name, ''))
+      return;
+    var li = document.createElement('li');
+    li.textContent = name;
+    li.addEventListener('click', TodoManager$addDone$lambda(this, li, name));
+    this.doneUl_0.appendChild(li);
+  };
+  TodoManager.prototype.clearTodoList = function () {
+    var tmp$;
+    while (this.todoUl_0.firstChild != null) {
+      if ((tmp$ = this.todoUl_0.firstChild) != null) {
+        this.todoUl_0.removeChild(tmp$);
+      }
+    }
+  };
+  TodoManager.prototype.clearDoneList = function () {
+    var tmp$;
+    while (this.doneUl_0.firstChild != null) {
+      if ((tmp$ = this.doneUl_0.firstChild) != null) {
+        this.doneUl_0.removeChild(tmp$);
+      }
+    }
   };
   TodoManager.$metadata$ = {
     kind: Kind_CLASS,
@@ -88,7 +101,6 @@ var TodoApp = function (_, Kotlin) {
     interfaces: []
   };
   _.main = main;
-  _.Task = Task;
   _.TodoManager = TodoManager;
   main();
   Kotlin.defineModule('TodoApp', _);
--- src/Main.kt
+++ src/Main.kt
@@ -8,27 +8,54 @@ fun main() {
         val doneUl = document.querySelector("#done_ul")
         val todoList = TodoManager(todoUl!!, doneUl!!)
 
-        document.querySelector("#button")?.addEventListener("click", {
-            println("click!")
+        // add dummy task
+        todoList.addTodo("今月の家賃を支払う")
+        todoList.addDone("洗剤を買う")
+        todoList.addDone("本棚を掃除する")
+
+        document.querySelector("#add_button")?.addEventListener("click", {
             val input = document.querySelector("#input") as HTMLInputElement
             todoList.addTodo("${input.value}")
             input.value = ""
         })
+        document.querySelector("#todo_clear_button")?.addEventListener("click", {
+            todoList.clearTodoList()
+        })
+        document.querySelector("#done_clear_button")?.addEventListener("click", {
+            todoList.clearDoneList()
+        })
     })
 }
 
-data class Task(val id: Int, val name: String, val isChecked: Boolean)
-
 class TodoManager(private val todoUl: Element, private val doneUl: Element) {
     fun addTodo(name: String) {
-        if (name == "") {
-            return
-        }
+        if (name == "") return
         val li = document.createElement("li")
         li.textContent = name
         li.addEventListener("click", {
-            println("click on $it")
+            todoUl.removeChild(li)
+            addDone(name)
         })
         todoUl.appendChild(li)
+    }
+    fun addDone(name: String) {
+        if (name == "") return
+        val li = document.createElement("li")
+        li.textContent = name
+        li.addEventListener("click", {
+            doneUl.removeChild(li)
+            addTodo(name)
+        })
+        doneUl.appendChild(li)
+    }
+    fun clearTodoList() {
+        while (todoUl.firstChild != null) {
+            todoUl.firstChild?.let { todoUl.removeChild(it) }
+        }
+    }
+    fun clearDoneList() {
+        while (doneUl.firstChild != null) {
+            doneUl.firstChild?.let { doneUl.removeChild(it) }
+        }
     }
 }
